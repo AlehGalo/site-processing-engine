9c95ba2fb654bcdbbc2878e7a4bf96a888a6666a
AlehGalo <galo1@tut.by>
2014-04-02 17:59:47 +0300

    Login page fix

--- crawler/src/main/java/com/jdev/crawler/core/process/LoginStepProcess.java
+++ crawler/src/main/java/com/jdev/crawler/core/process/LoginStepProcess.java
@@ -7,15 +7,12 @@ import static java.lang.String.format;
 
 import java.util.List;
 
-import org.apache.http.client.methods.HttpRequestBase;
-
 import com.jdev.crawler.core.process.extract.ISelectorExtractStrategy;
 import com.jdev.crawler.core.process.model.IEntity;
 import com.jdev.crawler.core.request.IRequestBuilder;
 import com.jdev.crawler.core.step.IStepConfig;
 import com.jdev.crawler.exception.CrawlerException;
 import com.jdev.crawler.exception.LoginException;
-import com.jdev.crawler.exception.SelectionException;
 
 /**
  * @author Aleh
@@ -50,22 +47,21 @@ public class LoginStepProcess extends AssembledStepProcess {
     public IEntity process(final IProcessSession session, final IEntity content,
             final ISelectorExtractStrategy selectorExtractStrategy) throws CrawlerException {
         IEntity cont = null;
+        int sizeBefore = 0;
         try {
+            sizeBefore = extractSelectors(session.getSessionContext(), selectorExtractStrategy,
+                    content).size();
             cont = super.process(session, content, selectorExtractStrategy);
         } catch (final CrawlerException e) {
             throw e;
         }
-        try {
-            HttpRequestBase request = createRequest(session.getSessionContext(),
-                    extractSelectors(session.getSessionContext(), selectorExtractStrategy, cont));
-            // TODO: change validation of this step.
-        } catch (final SelectionException se) {
-            // All if fine. Input elements are absent as a result login screen
-            // is ok.
-            return cont;
+        int size = extractSelectors(session.getSessionContext(), selectorExtractStrategy, cont)
+                .size();
+        if (size != sizeBefore) {
+            throw new LoginException(format("Logging in failed. For user=[%s].", session
+                    .getSessionContext().getUserData().getLogin()));
         }
-        throw new LoginException(format("Logging in failed. For user=[%s].", session
-                .getSessionContext().getUserData().getLogin()));
+        return cont;
     }
 
 }
