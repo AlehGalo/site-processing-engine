655fd45cd1d32dee4d61833c12dd3a5cf57097ea
AlehGalo <galo1@tut.by>
2014-06-16 23:51:30 +0300

    Liquibase changes

---
+++ domain/src/main/resources/liquibase/install/app/20140502_insert_site_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO SITE (SITE_ID, URL, RESOURCE_URL, DESCRIPTION) VALUES
+(1, "https://www.fl.ru/", "https://www.fl.ru/", "Russian Freelance Site"),
+(2, "http://www.freelancer.com", "http://www.freelancer.com/jobs/", "Multilanguage Freelance Site"),
+(3, "http://www.freelance.com", "http://www.freelance.com/en/search/mission", "One More Freelance Site");
---
+++ domain/src/main/resources/liquibase/install/app/20140519_insert_credentials_for_sites.sql
@@ -0,0 +1,4 @@
+INSERT INTO CREDENTIAL(USERNAME, PASSWORD, FK_SITE_ID) VALUES
+("informer-fl-ru", "aFGgR5435",1), 
+("InformerFL", "AfeDFe332Fef33fdd",2), 
+("informer-fl-com", "aFSsSR5435", 3);
